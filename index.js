97f3dce71ed5f69edbe2a7d0953e05762615a614
shaunvan119 <shaunvan@live.com.au>
2022-04-27 11:48:50 +1000

    all user data to dom

--- index.js
+++ index.js
@@ -15,9 +15,51 @@ formInput.addEventListener('submit', function(e){
 // fetching user data from git hub
 fetch('https://api.github.com/users/'+updatedSearch)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+      console.log(data)
+      document.getElementById("user-name").innerHTML = `
+      <h1>${data.login}</h1>
+      `
+      document.getElementById("user-img").innerHTML = `
+      <img src="${data.avatar_url}">
+      `
+      document.getElementById("user-bio").innerHTML = `
+      <p>${data.bio}</p>
+      `
+
+      document.getElementById("user-Joined-date").innerHTML = `
+      <p>${data.created_at}</p>
+      `
+
+      document.getElementById("user-repos").innerHTML = `
+      <p>${data.public_repos}</p>
+      `
+
+      document.getElementById("user-followers").innerHTML = `
+      <p>${data.followers}</p>
+      `
+
+      document.getElementById("user-following").innerHTML = `
+      <p>${data.following}</p>
+      `
+
+      document.getElementById("user-location").innerHTML = `
+      <p>${data.location}</p>
+      `
+
+      document.getElementById("user-github-blog").innerHTML = `
+      <p>${data.blog}</p>
+      `
+
+      document.getElementById("user-github-link").innerHTML = `
+      <p>${data.html_url}</p>
+      `
+
+
+  });
 
 })
+
 
 
 
