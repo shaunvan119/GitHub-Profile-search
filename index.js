2d2143121e9bfa81593a1862d559862358f509f6
shaunvan119 <shaunvan@live.com.au>
2022-04-27 15:23:59 +1000

    added if staement and refactored code

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 
 //html varibles
 let formInput = document.getElementById("formInput")
+    
 
 
-
-
-function renderContent() {
-
-}
 
 // event listener, listining for a submit
 formInput.addEventListener('submit', function(e){
@@ -23,7 +19,10 @@ fetch('https://api.github.com/users/'+updatedSearch)
   .then(response => response.json())
   .then(data => {
       console.log(data)
-      const renderElments = document.getElementById("main")
+    
+
+      if( updatedSearch == data.login) {
+      let renderElments = document.getElementById("main")
       renderElments.innerHTML = `
                 <h1>${data.login}</h1>
                 <img src="${data.avatar_url}">
@@ -36,47 +35,9 @@ fetch('https://api.github.com/users/'+updatedSearch)
                 <p>${data.blog}</p>
                 <p>${data.html_url}</p>
       `
-
-
-
-      /*document.getElementById("user-name").innerHTML = `
-      <h1>${data.login}</h1>
-      `
-      document.getElementById("user-img").innerHTML = `
-      <img src="${data.avatar_url}">
-      `
-      document.getElementById("user-bio").innerHTML = `
-      <p>${data.bio}</p>
-      `
-
-      document.getElementById("user-Joined-date").innerHTML = `
-      <p>${data.created_at}</p>
-      `
-
-      document.getElementById("user-repos").innerHTML = `
-      <p>${data.public_repos}</p>
-      `
-
-      document.getElementById("user-followers").innerHTML = `
-      <p>${data.followers}</p>
-      `
-
-      document.getElementById("user-following").innerHTML = `
-      <p>${data.following}</p>
-      `
-
-      document.getElementById("user-location").innerHTML = `
-      <p>${data.location}</p>
-      `
-
-      document.getElementById("user-github-blog").innerHTML = `
-      <p>${data.blog}</p>
-      `
-
-      document.getElementById("user-github-link").innerHTML = `
-      <p>${data.html_url}</p>
-      `*/
-
+    } else {
+                alert("NO USER FOUND")
+        }
 
   });
 
